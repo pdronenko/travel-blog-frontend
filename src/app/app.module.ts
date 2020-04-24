08ba6dd45ccc0b2a65184810fe2d8aa5c59cf952
Pavel Dronenko <p.dronenko@wellyes.ru>
2020-04-24 08:57:46 +0300

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { StoreModule } from '@ngrx/store'
-import { LayoutModule } from './modules/layout/layout.module';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { LayoutModule } from './modules/layout/layout.module'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { environment } from '../environments/environment'
 
 @NgModule({
