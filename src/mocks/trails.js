3881bd9b49436c627d1ca4e1ed51bb879fb72c4b
Pavel Dronenko <p.dronenko@wellyes.ru>
2020-05-21 08:32:50 +0300

    test commit

--- src/mocks/trails.js
+++ src/mocks/trails.js
@@ -1,6 +1,6 @@
 export default {
   "trails": [
-    { id: 1, name: "First trail test", pretext: "First pre text", date: new Date(), mainImgUrl: '/assets/images/forest-1.jpg' },
+    { id: 1, name: "First trail", pretext: "First pre text", date: new Date(), mainImgUrl: '/assets/images/forest-1.jpg' },
     { id: 2, name: "Second trail", pretext: "Second pre text", date: new Date(), mainImgUrl: '/assets/images/forest-2.jpg' },
     { id: 3, name: "Third trail", pretext: "Third pre text", date: new Date(), mainImgUrl: '/assets/images/forest-3.jpg' },
     { id: 4, name: "Second trail", pretext: "Second pre text", date: new Date(), mainImgUrl: '/assets/images/forest-2.jpg' },
